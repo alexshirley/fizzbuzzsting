ac82d7eeded7cad4f48170e775a9b3b8e4d6e0c0
Alex Shirley <alex.shirley@egym.de>
2020-07-12 16:01:51 +0200

    Add composable version

--- tests/fizzbuzz-bench.cpp
+++ tests/fizzbuzz-bench.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <fizzbuzz_basic_non_concat.h>
+#include <fizzbuzz_composable.h>
 
 static void FB_nonConcat(benchmark::State& state){
     for (auto _ : state)
@@ -9,5 +10,14 @@ static void FB_nonConcat(benchmark::State& state){
         }
 }
 BENCHMARK(FB_nonConcat);
+
+static void FB_composable(benchmark::State& state){
+    for (auto _ : state)
+        for (auto i = 1; i < 101; i++){
+            auto res = fizzbuzz_composable(i);
+            benchmark::DoNotOptimize(res);
+        }
+}
+BENCHMARK(FB_composable);
 
 BENCHMARK_MAIN();
--- tests/fizzbuzz-test.cpp
+++ tests/fizzbuzz-test.cpp
@@ -1,5 +1,6 @@
 #include <include/gtest/gtest.h>
 #include <fizzbuzz_basic_non_concat.h>
+#include <fizzbuzz_composable.h>
 
 namespace fizzbuzz{
 
@@ -7,9 +8,22 @@ class fizzbuzz_sanity_test :public ::testing::Test{
 
 };
 
-TEST_F(fizzbuzz_sanity_test, fizzbuzz_non_concat){
+TEST_F(fizzbuzz_sanity_test, fb_non_concat){
     auto should_be_fizz = fizzbuzz_basic_non_concat(3);
-    ASSERT_STREQ(should_be_fizz.c_str(),"Fizz");
+    EXPECT_STREQ(should_be_fizz.c_str(),"Fizz");
+    auto should_be_buzz = fizzbuzz_basic_non_concat(5);
+    EXPECT_STREQ(should_be_buzz.c_str(),"Buzz");
+    auto should_be_fizzbuzz = fizzbuzz_basic_non_concat(15);
+    EXPECT_STREQ(should_be_fizzbuzz.c_str(),"FizzBuzz");
+}
+
+TEST_F(fizzbuzz_sanity_test, fb_composable){
+    auto should_be_fizz = fizzbuzz_composable(3);
+    EXPECT_STREQ(should_be_fizz.c_str(),"Fizz");
+    auto should_be_buzz = fizzbuzz_composable(5);
+    EXPECT_STREQ(should_be_buzz.c_str(),"Buzz");
+    auto should_be_fizzbuzz = fizzbuzz_composable(15);
+    EXPECT_STREQ(should_be_fizzbuzz.c_str(),"FizzBuzz");
 }
 
 }
